8a8bfff4a8e27aa5bfc2e52463298a8989b7af08
Hanseeka-Dhingana <hanseeka.bscskdkf22@iba-suk.edu.pk>
2024-10-27 00:06:04 +0500

    <img> and <embed> tag

---
+++ img-embed.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image And Video file</title>
+
+    <style>
+        img{
+            height: 200px;
+            width: 200px; 
+            text-align:center;
+
+        }
+
+        embed{
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    
+  <img src="https://media.istockphoto.com/id/1127245421/photo/woman-hands-praying-for-blessing-from-god-on-sunset-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=Fbysb-YHuALb_Jyi4Q8Lw2QEVIFa3WSaPsvneGr7Q_8=" alt="">
+ 
+  <embed src="https://media.istockphoto.com/id/2002908406/video/abstract-underwater-bubbles.mp4?s=mp4-480x480-is&k=20&c=gqLfHzoEclK0NACysTNn6gpDdNEaeIBC0o0PvXSnEvA=" type="">
+</body> 
+</html>
